94ba5a7f8d4fccb117419c9ee8cf5b37feebdc9e
Ville-Markus Yli-Suutala <3628916+VeeEM@users.noreply.github.com>
2020-11-19 16:06:45 +0200

    Initial commit.

---
+++ Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import qualified Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Client.TLS as HTTP
+import qualified Network.HTTP.Types.Status as HTTP
+import qualified Network.HTTP.Types.Header as HTTP
+import qualified Data.ByteString.Lazy as BSL
+import System.Environment
+
+getWithUserAgent :: String -> IO (HTTP.Response BSL.ByteString)
+getWithUserAgent url = do
+    manager <- HTTP.newManager HTTP.tlsManagerSettings
+    req <- HTTP.parseRequest url
+    let request = req {
+        HTTP.requestHeaders = (HTTP.hUserAgent, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36") : []
+    }
+    HTTP.httpLbs request manager
+
+dlUrl :: String -> String -> String
+dlUrl prodver extid = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=" ++ prodver  ++ "&x=id%3D" ++ extid ++ "%26uc"
+
+saveResponse :: String -> String -> IO ()
+saveResponse url path = do
+    response <- getWithUserAgent url
+    let responseBody = HTTP.responseBody response
+    BSL.writeFile path responseBody
+
+main :: IO ()
+main = do
+  arg <- head <$> getArgs
+  if length arg /= 32
+     then putStrLn "Extension id must be 32 characters long."
+     else do
+       response <- getWithUserAgent $ dlUrl "83" arg
+       BSL.writeFile (arg ++ ".crx") (HTTP.responseBody response)
